dcca34828e2836adfea2968d8093cc07834901c7
Brian Levin <bml201095@gmail.com>
2020-03-21 01:23:48 -0400

    aaded a varible that holds the seconds value for the countdown and put a countdown interval within the events listener to begin the quiz when the user hits the start button

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ var questionFour = document.querySelector('.question4');
 var questionFive = document.querySelector('.question5');
 var finishedForm = document.querySelector('.finished form intials');
 var highScore = document.querySelector('.highscores');
+var countDown = document.querySelector('#countdown');
+
+var secondsLeft = 60;  // the seconds in the start of the countdown to complete the quiz.
+// added a event listener which allows the user to click the button to start the quiz.
+startQuizButton.addEventListener("click", function (event) {
+    event.preventDefault(); // added the event.prevent default  so the page will not refresh until the quiz is completed.
+
+    if (event.target.matches('button')) { // added a function within the if statement to start the time interval.
+        var timeInterval = setInterval(function () {
+            timerEl.textContent = timeLeft + " seconds remaining";
+            timeLeft--;
+            if (timeLeft === 0) {
+                countDown.textContent = ""; //  wanted to create an if statement once the countdown hits zero.
+
+                clearInterval(timeInterval);
+            }
+
+        }, 10000);
+
+    }
+
+}
+
+    }
