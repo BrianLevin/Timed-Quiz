7ec41f7b1ba5801914ced7d9cbc2a53ec3ea9b3b
Brian Levin <bml201095@gmail.com>
2020-03-20 23:23:07 -0400

    added some document.queryselctors on my div classes from my html page to utilize in my javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Made document.query selctors which targeted each of my div  classes in my html page  to utilize in javascript
+var startQuizButton = document.querySelector(".start");
+var questionOne = document.querySelector('.question 1');
+var questionTwo = document.querySelector('.question2');
+var questionThree = document.querySelector('.question3');
+var questionFour = document.querySelector('.question4');
+var questionFive = document.querySelector('.question5');
+var finishedForm = document.querySelector('.finished form intials');
+var highScore = document.querySelector('.highscores');
