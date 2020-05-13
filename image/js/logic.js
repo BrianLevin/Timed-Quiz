8187b86d3d68d8130b46e980b98a35e0a7c4f29c
Brian Levin <bml201095@gmail.com>
2020-05-13 14:33:55 -0400

    added a DOM variable which is going to hide the start screen

--- image/js/logic.js
+++ image/js/logic.js
@@ -13,7 +13,9 @@ var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
 function startQuix() {
-
+    // hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
 
 }
 
