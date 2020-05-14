8e8a4484d2f28c51879f9d158974d1c1725c7747
Brian Levin <bml201095@gmail.com>
2020-05-14 02:26:41 -0400

    added a variable which will get the local storage objects highscores or set to an empy array

--- image/js/scores.js
+++ image/js/scores.js
@@ -1,5 +1,6 @@
 function printHighscores() {
-
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
 
 }
